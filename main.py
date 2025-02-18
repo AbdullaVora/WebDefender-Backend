201cbca6adea6185270c0a98fd9843767dccdde8
Abdullah <abdullahvora136@gmail.com>
2025-02-19 01:04:49 +0530

    change main

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-from math import trunc
-from sys import prefix
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from config.database import db
 from routers.userRoute import router as user_router
-import uvicorn
-import os
+from mangum import Mangum  # ASGI Adapter for Vercel
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Only allow requests from your React app
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["POST","GET","PUT","PATCH","DELETE"],  # Allow all HTTP methods
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(user_router, prefix="/api/auth")
-
 
 @app.get("/")
 async def home():
     return {"message": "Server is running successfully"}
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+# ASGI Adapter for Vercel
+handler = Mangum(app)

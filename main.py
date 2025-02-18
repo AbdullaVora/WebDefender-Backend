772cde86bc255359cf23110d6a163880e3b53167
Abdullah <abdullahvora136@gmail.com>
2025-02-19 00:32:30 +0530

    change port

--- main.py
+++ main.py
@@ -25,5 +25,5 @@ async def home():
     return {"message": "Server is running successfully"}
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Use PORT from Render, default to 8000 if not set
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)

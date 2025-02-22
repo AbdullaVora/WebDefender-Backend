4d5232e4fe18eaf18daa220596607bb65b155817
Abdullah <abdullahvora136@gmail.com>
2025-02-22 19:14:47 +0530

    add cors

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(user_router, prefix="/api/auth")
 
